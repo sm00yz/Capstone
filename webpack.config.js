6d6e23693c83b91c77e6b8eeeaf0f8ddd555931a
Samia <alghamdi.smasm@gmail.com>
2020-07-16 14:14:26 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
@@ -41,6 +42,16 @@ module.exports = {
             filename: "./index.html",
         }),
         new MiniCssExtractPlugin({ filename: "[name].css" }),
+
+        new CleanWebpackPlugin({
+          // Simulate the removal of files
+          dry: true,
+          // Write Logs to Console
+          verbose: true,
+          // Automatically remove all unused webpack assets on rebuild
+          cleanStaleWebpackAssets: true,
+          protectWebpackAssets: false
+      }),
 
         new WorkboxPlugin.GenerateSW()
 
